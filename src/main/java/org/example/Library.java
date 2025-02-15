047a20b0dd811494eab9e2043b8395b1915ff676
Гулназым Булжан <gulnazymbulzan@192.168.10.8>
2025-02-15 15:24:03 +0500

    Add Book Library and Book

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class Library {
+    private final List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" + "books=" + books + '}';
+    }
 }
